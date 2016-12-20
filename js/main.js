d57e908b0a03a45aa4923cc81e8890d50ed5df24
HolyAnger <Astral.the.best@gmail.com>
2016-12-21 01:11:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by HolyAnger on 20.12.2016.
+ */
+$(document).ready(function() {
+    $('#myImage').css({'display':'block', 'animation':'left .9s linear'});
+    $('#myInfo').css({'display':'block', 'animation':'right .9s linear'});
+    $('#link').css({'display':'block', 'animation':'height .9s linear'});
+});
