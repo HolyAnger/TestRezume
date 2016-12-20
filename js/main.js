d3399a24a71d719edfad54ce072eb4a1cf519282
HolyAnger <Astral.the.best@gmail.com>
2016-12-21 02:17:51 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Created by HolyAnger on 20.12.2016.
  */
 $(document).ready(function() {
-    $('#myImage').css({'animation':'left .9s .1s linear', 'display':'block'});
-    $('#myInfo').css({'animation':'right .9s .1s linear', 'display':'block'});
-    $('#link').css({'animation':'height .9s .1s linear', 'display':'block'});
+    $('#myImage').css({'animation':'left .9s .2s linear', 'display':'block'});
+    $('#myInfo').css({'animation':'right .9s .2s linear', 'display':'block'});
+    $('#link').css({'animation':'height .9s .2s linear', 'display':'block'});
 });
